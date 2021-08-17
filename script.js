81fc69284ad6cd21f23823aba88d75e7ce91d4b0
Andres <andres.paredes@protonmail.com>
2021-08-16 20:39:20 -0400

    Studio Bonus Done!

--- script.js
+++ script.js
@@ -5,16 +5,18 @@ window.addEventListener('load', function(){
         response.json().then( function( astronauts ){
             
             console.log( astronauts );
+            astronauts.sort( (a1, a2) => a1.hoursInSpace - a2.hoursInSpace);
             
             const Astro = document.getElementById("container");
 
+            let index = 1;
             const htmlAstronaut = astronauts.forEach( astronaut => Astro.innerHTML +=
                 `<div class="astronaut">
                 <div class="bio">
-                   <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+                   <h3>${index++}. ${astronaut.firstName} ${astronaut.lastName}</h3>
                    <ul>
                       <li>Hours in space: ${astronaut.hoursInSpace}</li>
-                      <li>Active: ${astronaut.active}</li>
+                      <li style=color:${ astronaut.active ? "green" : "" }>Active: ${astronaut.active}</li>
                       <li>Skills: ${astronaut.skills} </li>
                    </ul>
                 </div>
